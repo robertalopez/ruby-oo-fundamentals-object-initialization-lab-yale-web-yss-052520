46ab8e2674d002452ec8568ab826e6d5a7151a21
robertalopez <roblop1234@yahoo.com>
2020-05-25 16:42:58 -0700

    done

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person 
+     def initialize(nombre)
+        @name = nombre 
+    end 
+
+    # def name(nombre)
+        #@name = nombre 
+    # end 
+
+    # def name
+    #     @name
+    # end 
+end 
